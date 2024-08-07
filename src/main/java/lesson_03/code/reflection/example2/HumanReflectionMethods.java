5b1feff9e11930f9e7dff97db0424c6ce78373e4
StanislavPB <perminovsb@gmail.com>
2024-08-07 20:59:33 +0100

    lesson 3

---
+++ src/main/java/lesson_03/code/reflection/example2/HumanReflectionMethods.java
@@ -0,0 +1,28 @@
+package lesson_03.code.reflection.example2;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class HumanReflectionMethods {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+
+        Human john = new Human("John","London");
+
+        Class humanClass = john.getClass();
+
+        Method[] methods = humanClass.getDeclaredMethods();
+
+        for (Method method : methods) {
+            System.out.println(method);
+
+            if (method.getName().contains("Secret")){
+                System.out.println("Find private method!");
+                method.setAccessible(true);
+                int secretCode = (int) method.invoke(john,"Our method was broken");
+
+                System.out.println(secretCode);
+            }
+        }
+
+    }
+}
