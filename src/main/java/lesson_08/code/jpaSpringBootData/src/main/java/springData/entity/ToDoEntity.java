1432e8afee43d1b4dbec9a991142ae2165ed288d
StanislavPB <perminovsb@gmail.com>
2024-08-19 20:55:58 +0100

    lesson 08

--- src/main/java/lesson_08/code/jpaSpringBootData/src/main/java/springData/entity/ToDoEntity.java
+++ src/main/java/lesson_08/code/jpaSpringBootData/src/main/java/springData/entity/ToDoEntity.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "todo")
 @Data
@@ -19,4 +21,17 @@ public class ToDoEntity {
     private String name;
 
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "to_do_author_id")
+    private User toDoAuthor;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "todo_collaborates", //имя таблицы связи
+            joinColumns = @JoinColumn(name = "todo_id"), //имя столбца для ToDoEntity
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> collaborate;
 }
