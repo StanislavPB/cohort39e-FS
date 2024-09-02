4328a9b26d837b0e53d15789d23431408f6b635f
StanislavPB <perminovsb@gmail.com>
2024-09-02 23:21:49 +0100

    add: lesson 14

---
+++ src/main/java/lesson_14/code/demoShop39/src/test/java/org/demoshop39/DemoShop39ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demoshop39;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoShop39ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
